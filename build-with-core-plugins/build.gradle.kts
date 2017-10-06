41be7c7f6d63da447fdddce922d4361035525447
Paul Merlin <paul@gradle.com>
2017-10-06 10:59:53 +0200

    Update set of built-in plugins
    New native plugins are commented out as their application fails
    See https://github.com/gradle/gradle/pull/3101

--- build-with-core-plugins/build.gradle.kts
+++ build-with-core-plugins/build.gradle.kts
@@ -17,36 +17,77 @@ plugins {
     `project-report`
     `project-reports`
     ear
-    scala
+    `visual-studio`
+    xcode
+    `play-ide`
     `ivy-publish`
     jacoco
+    `coffeescript-base`
     envjs
+    `javascript-base`
     jshint
     rhino
+    `java-lang`
+    `jvm-resources`
+    `assembler-lang`
     assembler
+    `c-lang`
     c
+    // `cpp-executable`
+    `cpp-lang`
+    // `cpp-library`
     cpp
+    `objective-c-lang`
     `objective-c`
+    `objective-cpp-lang`
     `objective-cpp`
+    // `swift-executable`
+    // `swift-library`
+    `windows-resource-script`
     `windows-resources`
+    `scala-lang`
     `maven-publish`
     maven
     osgi
+    `binary-base`
+    `component-base`
+    `component-model-base`
+    `language-base`
+    `lifecycle-base`
+    `jvm-component`
+    `clang-compiler`
+    `gcc-compiler`
+    `microsoft-visual-cpp-compiler`
+    `native-component-model`
+    `native-component`
+    `standard-tool-chains`
+    `play-application`
+    `play-coffeescript`
+    `play-javascript`
     play
     `java-gradle-plugin`
     application
+    base
     distribution
-    java
-    `java-library`
-    `java-library-distribution`
+    `groovy-base`
     groovy
+    `java-base`
+    `java-library-distribution`
+    `java-library`
+    java
     war
     publishing
     `build-dashboard`
+    `reporting-base`
+    `scala-base`
+    scala
     signing
     `junit-test-suite`
+    `cunit-test-suite`
     cunit
+    `google-test-test-suite`
     `google-test`
+    // xctest
 }
 
 val clean: org.gradle.api.tasks.Delete by tasks
